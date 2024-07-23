d878c2216c8042e3c2ffb15423cd3b47d23b4610
AkinNetwork <margaretasandor@gmail.com>
2024-07-23 12:18:48 +0100

    Extend Vite Network for cross device testing

--- UIdev/UIdev.react/eRunnerGUI/vite.config.js
+++ UIdev/UIdev.react/eRunnerGUI/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Bind to all network interfaces
+    port: 3000, // Specify the port, adjust if needed
+    strictPort: true, // Prevent Vite from using a different port if the specified port is already in use
+    // Optionally, you can add other configurations here
+  },
+});
